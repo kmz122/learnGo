7d3a03038dc352d45fed957f2aafef2b0f37f111
kmz122 <65806780+kmz122@users.noreply.github.com>
2024-02-21 13:53:23 +0630

    Return a random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,8 +3,10 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
+// Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
 	// If no name was given, return an error with a message.
 	if name == "" {
@@ -13,6 +15,23 @@ func Hello(name string) (string, error) {
 
 	// If a name was received, return a value that embeds the name
 	// in a greeting message.
-	message := fmt.Sprintf("Hi, %v! Welcome.", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// A Go's slice is like an array, except that its size changes dynamically as you add and remove items. The slice is one of Go's most useful types.
+// randomFormat() not exported the func, local
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
