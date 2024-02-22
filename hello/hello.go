795f40a8c4e7fca13af09c485e57079363d1da35
kmz122 <65806780+kmz122@users.noreply.github.com>
2024-02-22 21:40:47 +0630

    Return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -25,4 +25,16 @@ func main() {
 	// If no error was returned, print the returned message
 	// to the console.
 	fmt.Println(message)
+
+	// A slice of names.
+	names := []string{"Kyi", "Myo", "Zaw"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Printf("Greeting messages: %v", messages)
 }
